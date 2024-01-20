64685d0e1d236ae1b394b3b3263713002eb52c5e
Nemanja Milenkovic <nemanjamilenkovic@live.com>
2024-01-20 22:59:54 +0100

    Extend the interface
    - Add additional methods to the interface
    - Implement Get method  in the basic implementation

--- DictionaryList/DictionaryList.cs
+++ DictionaryList/DictionaryList.cs
@@ -7,7 +7,9 @@ namespace DictionaryList
     public class DictionaryList<Tkey, TValue> : IDictionaryEnumerable<Tkey, TValue>
     {
         private Dictionary<Tkey, List<TValue>> _internalDictionary = new Dictionary<Tkey, List<TValue>>();
-        
+
+        public int Count => throw new NotImplementedException();
+
         public void Add(Tkey key, TValue value)
         {
             if (_internalDictionary.ContainsKey(key)) 
@@ -18,6 +20,36 @@ namespace DictionaryList
             {
                 _internalDictionary.Add(key, new List<TValue>() {value});
             }
+        }
+
+        public void AddMultiple(Tkey key, IEnumerable<TValue> value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CountElementsByKey(Tkey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(Tkey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TValue> Get(Tkey key)
+        {
+            if (_internalDictionary.TryGetValue(key, out var values)) 
+            {
+                return values;
+            }
+
+            return new List<TValue>();
+        }
+
+        public void Remove(Tkey key)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- DictionaryList/IDictionaryEnumerable.cs
+++ DictionaryList/IDictionaryEnumerable.cs
@@ -1,7 +1,16 @@
-﻿namespace DictionaryList
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace DictionaryList
 {
     public interface IDictionaryEnumerable<Tkey, TValue>
     {
         void Add(Tkey key, TValue value);
+        void AddMultiple(Tkey key, IEnumerable<TValue> value);
+        void Remove(Tkey key);
+        bool Exists(Tkey key);
+        IEnumerable<TValue> Get(Tkey key);
+        int Count { get; }
+        int CountElementsByKey(Tkey key);
     }
 }
